32e379e5653534f3c90b0bbf8d06d636de1e890a
Sugibiz <m.sugimoto@cellpromote.biz>
2023-09-01 04:07:04 +0900

    民宿の登録機能を作成

--- src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
+++ src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
@@ -6,14 +6,20 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.samuraitravel.entity.House;
 import com.example.samuraitravel.form.HouseRegisterForm;
 import com.example.samuraitravel.repository.HouseRepository;
+import com.example.samuraitravel.service.HouseService;
 
 //①　クラスに@RequestMappingアノテーションをつけ、ルートパスの基準値を設定する
 
@@ -34,20 +40,25 @@ import com.example.samuraitravel.repository.HouseRepository;
 //ただし、今回のようにコンストラクタが1つしかない場合は以下のように省略が可能です。
 //DIコンテナに登録されたインスタンスをコンストラクタなどに対して提供（注入）するのが@Autowiredアノテーションの役割です。
 public class AdminHouseController {
-	private final HouseRepository houseRepository;         
+	private final HouseRepository houseRepository;
+	private final HouseService houseService;
 
-	public AdminHouseController(HouseRepository houseRepository) {
-		this.houseRepository = houseRepository;                
-	}	
+	public AdminHouseController(HouseRepository houseRepository, HouseService houseService) {
+		this.houseRepository = houseRepository;
+		this.houseService = houseService;
+	}
+
 	//③
 	@GetMapping
-	public String index(Model model, @PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable, @RequestParam(name = "keyword", required = false) String keyword) {	
+	public String index(Model model,
+			@PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable,
+			@RequestParam(name = "keyword", required = false) String keyword) {
 		Page<House> housePage;
 		//④メソッド内でaddAttribute()メソッドを使い、以下の引数を渡す
 		//第1引数：ビュー側から参照する変数名　第2引数：ビューに渡すデータ
-		if(keyword != null && !keyword.isEmpty()) {
+		if (keyword != null && !keyword.isEmpty()) {
 			housePage = houseRepository.findByNameLike("%" + keyword + "%", pageable);
-		}else {
+		} else {
 			housePage = houseRepository.findAll(pageable);
 		}
 		model.addAttribute("housePage", housePage);
@@ -57,18 +68,27 @@ public class AdminHouseController {
 	}
 
 	@GetMapping("/{id}")
-	public String show(@PathVariable(name = "id")Integer id, Model model) {
+	public String show(@PathVariable(name = "id") Integer id, Model model) {
 		House house = houseRepository.getReferenceById(id);
 
 		model.addAttribute("house", house);
 
 		return "admin/houses/show";
 	}
-	
-	   @GetMapping("/register")
-	     public String register(Model model) {
-	         model.addAttribute("houseRegisterForm", new HouseRegisterForm());
-	         return "admin/houses/register";
-	     } 
-}
 
+	@GetMapping("/register")
+	public String register(Model model) {
+		model.addAttribute("houseRegisterForm", new HouseRegisterForm());
+		return "admin/houses/register";
+	}
+	
+	@PostMapping("/create")
+	public String create(@ModelAttribute @Validated  HouseRegisterForm houseRegisterForm, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+		if(bindingResult.hasErrors()) {
+			return "admin/houses/register";
+			}
+		houseService.create(houseRegisterForm);
+		redirectAttributes.addFlashAttribute("successMessage","民宿を登録しました。");
+		return "redirect:/admin/houses";
+}
+}
